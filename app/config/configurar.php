b23780bd0b97267a14698d8a4bfd20bb979e7a1d
TenergyMX <tenergy.mx@gmail.com>
2023-12-04 10:54:27 -0600

    configuracion

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,6 +1,6 @@
 <?php
 	// conexion con la base de datos
-	define('DB_HOST','localhost');
+	define('DB_HOST','localhost:3307');
 	define('DB_USER','root');
 	define('DB_PASSWORD','');
 	define('DB_NAME','sfvi');
